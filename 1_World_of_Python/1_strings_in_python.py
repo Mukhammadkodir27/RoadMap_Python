589e7fea55704f8d8d47d6695bff0bb5de1632a3
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-09-06 06:54:10 +0200

    Update 1_strings_in_python.py

--- 1_World_of_Python/1_strings_in_python.py
+++ 1_World_of_Python/1_strings_in_python.py
@@ -1,8 +1,1 @@
-print("Hello I have been looking for you")
-print("Hello " + " World!")
 
-his_name = "Karim"
-his_country = "Uzbekistan"
-print(his_name + " " + his_country)
-name = input("Enter your name: ")
-print(f"Hello {name}")
