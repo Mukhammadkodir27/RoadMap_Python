93557eeabd9788eb3be99a0abae79163365a0822
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-09-07 09:11:43 +0200

    Update 2_str_String_Data_Type.py

--- 1_World_of_Python/2_str_String_Data_Type.py
+++ 1_World_of_Python/2_str_String_Data_Type.py
@@ -1,8 +1,26 @@
 parrot = "Norwegian Blue"
-# 01234567890123
+#         01234567890123         14 chars in this string
+#   -14,-13,-12,-11 ... ,-3,-2,-1
 print(parrot[3])
 print(parrot[4])
 print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+print("---------------------------------")
+#! negative indexing in strings
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+#! Trick to divide pos n to max element. here it is 14
+print("---------------------------------")
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print(parrot[9 - 14])
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+# ? the results are same # we win
